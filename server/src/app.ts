7846ead842e24ec8edb29b3dea164a6f7a10ea83
ALIyakuboff <asdovasd446@gmail.com>
2026-02-27 16:44:38 +0500

    feat: increase rate limit to 800

--- server/src/app.ts
+++ server/src/app.ts
@@ -45,10 +45,10 @@ app.use(cors({
 // Apply basic security headers
 app.use(helmet());
 
-// Apply rate limiting (Max 150 requests per 15 minutes)
+// Apply rate limiting (Max 800 requests per 15 minutes)
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 150, // Limit each IP to 150 requests per `window`
+    max: 800, // Limit each IP to 800 requests per `window`
     handler: (req: express.Request, res: express.Response) => {
         res.status(429).json({ error: 'Juda ko\'p so\'rov yuborildi. Iltimos, 15 daqiqadan so\'ng qayta urinib ko\'ring.' });
     },
