77f84317cd476f9359c068293634d463f66f768f
ALIyakuboff <asdovasd446@gmail.com>
2026-03-09 12:32:23 +0500

    chore(android): update capacitor build and main activity for plugin registration

--- client/android/app/src/main/java/com/ziyokor/education/MainActivity.java
+++ client/android/app/src/main/java/com/ziyokor/education/MainActivity.java
@@ -1,14 +1,21 @@
 package com.ziyokor.education;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import java.util.ArrayList;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         registerPlugin(UsageStatsPlugin.class);
+
+        // Enable WebView Caching for Offline Support
+        WebView webView = getBridge().getWebView();
+        WebSettings settings = webView.getSettings();
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
     }
 }
